cbc78fa1238414aa40c35c37e224dcd95539ad91
Amarnath S <55694299+amarbhai@users.noreply.github.com>
2021-05-30 22:06:22 +0530

    Changed the address comment

--- Ethereum/election_factory.js
+++ Ethereum/election_factory.js
@@ -3,7 +3,7 @@ import ElectionFactory from './Build/ElectionFact.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(ElectionFactory.interface),
-    '0x51ac40ae4333BaD51308c376383805De1Dc06EA3'
+    'NEED TO ADD THE ADDRESS YOU DEPLOY'
 );
 
 export default instance;
